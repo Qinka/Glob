822290ec1650e731c2dc547a83af9fbb23837149
Qinka <qinka@live.com>
2016-02-08 22:21:23 +0800

    update

--- database/table.sql
+++ database/table.sql
@@ -10,7 +10,7 @@ CREATE TABLE table_nav
   key_label TEXT NOT NULL PRIMARY KEY,
   key_order INT ,
   key_ref TEXT NOT NULL,
-  key_time TIMESTAMP NOT NULL
+  key_time TIMESTAMP WITH TIME ZONE NOT NULL
 );
 
 CREATE TABLE table_html
@@ -19,7 +19,7 @@ CREATE TABLE table_html
   key_html TEXT NOT NULL,
   key_title TEXT NOT NULL,
   key_content TEXT NOT NULL,
-  key_time TIMESTAMP NOT NULL
+  key_time TIMESTAMP WITH TIME ZONE NOT NULL
 );
 
 CREATE TABLE table_txt
@@ -27,7 +27,7 @@ CREATE TABLE table_txt
   key_index TEXT NOT NULL PRIMARY KEY,
   key_text Text NOT NULL,
   key_content Text NOT NULL,
-  key_time TIMESTAMP NOT NULL
+  key_time TIMESTAMP WITH TIME ZONE NOT NULL
 );
 
 CREATE TABLE table_bin
@@ -35,12 +35,12 @@ CREATE TABLE table_bin
   key_index TEXT NOT NULL PRIMARY KEY,
   key_binary BYTEA NOT NULL,
   key_content TEXT NOT NULL,
-  key_time TIMESTAMP NOT NULL
+  key_time TIMESTAMP WITH TIME ZONE NOT NULL
 );
 
 CREATE TABLE table_query
 (
   key_index TEXT NOT NULL PRIMARY KEY,
   key_text TEXT NOT NULL,
-  key_time TIMESTAMP NOT NULL
+  key_time TIMESTAMP WITH TIME ZONE NOT NULL
 );
