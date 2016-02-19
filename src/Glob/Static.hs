5bf6639f79716e2169311cb47db8a2c05d689b43
Qinka <qinka@live.com>
2016-02-19 14:38:05 +0800

    update

---
+++ src/Glob/Static.hs
@@ -0,0 +1,35 @@
+
+
+
+
+
+-- src/Glob/Static.hs
+
+
+module Glob.Static
+    ( getStaticR
+    ) where
+
+      import Glob.Data
+      import Glob.Common
+      import Glob.Config
+      import Yesod
+
+      import System.Directory
+      import Data.Text
+      import qualified Data.ByteString as BIO
+
+      getStaticR :: [Text] ->  Handler TypedContent
+      getStaticR sp' = do
+        path' <- fmap (staticPath.config) getYesod
+        let sp = ws2s sp'
+        let path = path' ++ t2s sp
+        fileExist path $ do
+          fileMIME <- liftIO $ BIO.readFile $ path ++ ".mime"
+          sendFile fileMIME path
+        where
+          fileExist path doItem = do
+            isE <- liftIO $ doesFileExist path
+            if isE
+              then doItem
+              else notFound
