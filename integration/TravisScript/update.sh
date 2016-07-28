0a8cab5f21205ce44e87691b24996b81589ddcc8
Qinka <qinka@live.com>
2016-07-28 11:34:50 +0800

    update, fight with ci

--- integration/TravisScript/update.sh
+++ integration/TravisScript/update.sh
@@ -22,5 +22,8 @@ cp ~/.local/bin/glob-launch docker.tmp/bin
 cp $TRAVIS_BUILD_DIR/integration/Dockerfiles/hub/Dockerfile dockrt.tmp
 cp $TRAVIS_BUILD_DIR/integration/ShellScript/start.sh docker.tmp/bin
 echo build docker
-cd docker.tmp && docker build -t qinka/glob:$DOCKER_IMAGE_TAG . && cd ..
+cd docker.tmp
+pwd
+ls
+docker build -t qinka/glob:$DOCKER_IMAGE_TAG . && cd ..
 docker push  qinka/glob
