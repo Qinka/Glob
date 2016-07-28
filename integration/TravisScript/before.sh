4b881b9d5b68d1b0941be4852ac08fc1280ec89c
Qinka <qinka@live.com>
2016-07-28 09:17:45 +0800

    update, fight with ci

--- integration/TravisScript/before.sh
+++ integration/TravisScript/before.sh
@@ -2,8 +2,8 @@
 echo pre-install
 ########
 echo update apt
-apt update
-apt install wget
+sudo apt update
+sudo apt install wget
 ######
 echo fetch the system\' name
 export OS_CORENAME=$(lsb_release -a | grep Codename | awk '{print $2}')
@@ -17,6 +17,7 @@ if [ -n "$LLVM" ]; then
   echo deb-src http://apt.llvm.org/$OS_CORENAME/ llvm-toolchain-$OS_CORENAME main | sudo tee -a /etc/apt/sources.list.d/llvm.list
   echo deb http://apt.llvm.org/$OS_CORENAME/ llvm-toolchain-$OS_CORENAME-$LLVM main | sudo tee -a /etc/apt/sources.list.d/llvm.list
   echo deb-src http://apt.llvm.org/$OS_CORENAME/ llvm-toolchain-$OS_CORENAME-$LLVM main | sudo tee -a /etc/apt/sources.list.d/llvm.list
+  sudo apt update
   sudo apt install -y libllvm-$LLVM-ocaml-dev libllvm$LLVM libllvm$LLVM-dbg lldb-$LLVM llvm-$LLVM llvm-$LLVM-dev llvm-$LLVM-runtime lldb-$LLVM-dev
 else
   echo without llvm
@@ -27,7 +28,8 @@ docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 ######
 echo install stack
 mkdir -p ~/.local/bin
-export PATH=$HOME/.local/bin:$PATHtravis_retry curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack'
+export PATH=$HOME/.local/bin:$PATH
+travis_retry curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack'
 echo setting up ghc
 export PATH=/opt/ghc/$GHC_VER/bin:$PATH
 ######
