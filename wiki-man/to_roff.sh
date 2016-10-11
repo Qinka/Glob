5bce12715929b5d2499d290f7961cab20d7c9b48
Qinka <qinka@live.com>
2016-10-11 15:52:42 +0800

    update

--- wiki-man/to_roff.sh
+++ wiki-man/to_roff.sh
@@ -3,5 +3,5 @@
 # To translate Markdown to *roff(troff).
 
 ls -1 *.md | awk -F '.' '{print "pandoc -s -t man " $0 " -o " ".ignore/"$2"."$1"\ncp .ignore/"$2"."$1" /usr/share/man/man"$1}' | bash
-cp .ignore/* /usr/share/man/man
+# cp .ignore/* /usr/share/man/man
 
