6fb1b0771ecf8bc72068308e09e249ba5cb3209b
Qinka <qinka@live.com>
2017-08-12 21:21:27 +0800

    update

--- glob-tool/src/Glob/Tool/Script.hs
+++ glob-tool/src/Glob/Tool/Script.hs
@@ -5,20 +5,38 @@ module Glob.Tool.Script
   ) where
 
 import           Glob.Tool.Opt
+import           System.FilePath
 import           System.IO
 
 scriptHandler :: Glob -> IO ()
 scriptHandler Script{..} = do
   case sptKind of
-    Nothing            -> hPutStrLn stderr "help"
-    Just "cfg-upgrade" -> putStrLn cfgUpgrade
+    Just "cfg-upgrade" -> do
+      ls <- lines <$> getContents
+      mapM_ (putStrLn . cfgUpgrade) ls
+    Just "cfg-rename" -> do
+      ls <- lines <$> getContents
+      mapM_ (putStrLn . cfgRename) ls
+    Just "make-all" -> putStrLn makeUpdate
+    _                  -> hPutStrLn stderr "help"
 
 
-cfgUpgrade :: String
-cfgUpgrade =  "sed 's/\"html\"/\"content\"/g'"
+
+cfgUpgrade :: String -> String
+cfgUpgrade filepath  =  "cat " ++ filepath
+  ++ " | " ++ "sed 's/\"html\"/\"content\"/g'"
   ++ " | " ++ "sed 's/\"binary\"/\"content\"/g'"
   ++ " | " ++ "sed 's/\"text\"/\"content\"/g'"
   ++ " | " ++ "sed 's/\"url\"/\"content\"/g'"
   ++ " | " ++ "sed 's/\"var\"/\"content\"/g'"
   ++ " | " ++ "sed 's/\"create-time\"/\"cretime\"/g'"
+  ++ " > " ++ filepath
 
+
+cfgRename :: String -> String
+cfgRename file =
+  let (name,ext) = splitExtension file
+  in name++ext++" "++name++".item"++ext
+
+makeUpdate :: String
+makeUpdate = "ls -1 .glob/*.item.json | awk -F/ '{print $2}' | sed  's/.item.json//g' | awk '{print \"make \"$1\" SITE_URL=$SITE_URL SITE_TOKEN=`glob ih -h sha256 -t $SITE_TOKEN ` \"}'  | sh"
